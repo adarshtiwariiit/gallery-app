0e3a3fca0b38333b2680887a6f52882a43fda475
ADARSH TIWARI <70533581+adarshtiwariiit@users.noreply.github.com>
2022-07-08 18:32:20 +0530

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/com/example/anujsharma/manageit/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/anujsharma/manageit/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.anujsharma.manageit;
+package com.example.adarshtiwai.manageit;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.anujsharma.manageit", appContext.getPackageName());
+        assertEquals("com.example.adarshtiwari.manageit", appContext.getPackageName());
     }
 }
